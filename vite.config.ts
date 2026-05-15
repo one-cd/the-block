34aee667f7cc224fff91e9ec3434eaa999c70430
one-cd <abhishek.koka@gmail.com>
2026-05-14 22:02:41 -0400

    Add test tooling

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,17 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: "jsdom",
+    environmentOptions: {
+      jsdom: {
+        url: "http://localhost/",
+      },
+    },
+    setupFiles: "./src/test/setup.ts",
+    css: true,
+    include: ["src/**/*.test.{ts,tsx}"],
+  },
 });
